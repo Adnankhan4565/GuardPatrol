0c5c671fe3169189551c49da2d5eed63c4e013d7
Adnan Khan <IGNITION420@GMAIL.COM>
2025-05-30 17:11:03 +0500

    mono setup

---
+++ apps/mobile-app/app/_layout.tsx
@@ -0,0 +1,38 @@
+import { useFrameworkReady } from '@/hooks/useFrameworkReady';
+import { ConvexAuthProvider } from "@convex-dev/auth/react";
+import { ConvexReactClient } from "convex/react";
+import { Stack } from 'expo-router';
+import * as SecureStore from "expo-secure-store";
+import { StatusBar } from 'expo-status-bar';
+import { Platform } from 'react-native';
+
+const convex = new ConvexReactClient(process.env.EXPO_PUBLIC_CONVEX_URL!, {
+  unsavedChangesWarning: false,
+});
+
+const secureStorage = {
+  getItem: SecureStore.getItemAsync,
+  setItem: SecureStore.setItemAsync,
+  removeItem: SecureStore.deleteItemAsync,
+};
+
+export default function RootLayout() {
+  useFrameworkReady();
+
+  return (
+ <ConvexAuthProvider
+      client={convex}
+      storage={
+        Platform.OS === "android" || Platform.OS === "ios"
+          ? secureStorage
+          : undefined
+      }
+    >
+      <Stack screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      </Stack>
+      <StatusBar style="auto" />
+  </ConvexAuthProvider>
+  );
+}
