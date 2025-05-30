81c61164e692e6d5f6be5762d319e3f5a2e725f1
Adnan Khan <IGNITION420@GMAIL.COM>
2025-05-30 18:58:00 +0500

    implement Convex API and schema with authentication, HTTP routing, and function definitions

---
+++ packages/backend/convex/schema.ts
@@ -0,0 +1,27 @@
+import { defineSchema, defineTable } from "convex/server";
+import { v } from "convex/values";
+import { authTables } from "@convex-dev/auth/server";
+ 
+const schema = defineSchema({
+  ...authTables,
+  users: defineTable({
+    name: v.optional(v.string()),
+    image: v.optional(v.string()),
+    email: v.optional(v.string()),
+    role: v.optional(v.string()),
+    emailVerificationTime: v.optional(v.number()),
+    phone: v.optional(v.string()),
+    phoneVerificationTime: v.optional(v.number()),
+    isAnonymous: v.optional(v.boolean()),
+    // other "users" fields...
+  }).index("email", ["email"]),
+  
+  // Your other tables...
+    numbers: defineTable({
+    value: v.number(),
+  }),
+});
+ 
+export default schema;
+
+
