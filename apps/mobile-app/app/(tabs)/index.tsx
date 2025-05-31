a469eb42e980446cbdd9c0be264034bdad39f006
Adnan khan <IGNITION420@GMAIL.COM>
2025-05-31 18:38:45 +0500

    logout

--- apps/mobile-app/app/(tabs)/index.tsx
+++ apps/mobile-app/app/(tabs)/index.tsx
@@ -1,4 +1,4 @@
-import { View, Text, StyleSheet, Pressable, Keyboard } from "react-native";
+import { View, Text, StyleSheet, Pressable } from "react-native";
 import { Scan } from "lucide-react-native";
 import {
   Authenticated,
